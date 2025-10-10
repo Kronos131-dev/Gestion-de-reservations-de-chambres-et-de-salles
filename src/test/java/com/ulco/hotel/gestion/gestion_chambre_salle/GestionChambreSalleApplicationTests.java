e0802eb74d0138948fbf87f1d57a605cc0b94cbf
FloDoyer <doyer.florent@icloud.com>
2025-10-10 14:25:06 +0200

    setup

---
+++ src/test/java/com/ulco/hotel/gestion/gestion_chambre_salle/GestionChambreSalleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulco.hotel.gestion.gestion_chambre_salle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionChambreSalleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
