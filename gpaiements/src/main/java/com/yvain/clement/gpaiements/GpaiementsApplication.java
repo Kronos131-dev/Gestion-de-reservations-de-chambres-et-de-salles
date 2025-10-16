ff7bbe43a873f75555602f1e18caa9a5b4b0fc89
Kronos <yvaintellier@etu.univ-littoral.fr>
2025-10-16 11:31:35 +0200

    initialisation du module

---
+++ gpaiements/src/main/java/com/yvain/clement/gpaiements/GpaiementsApplication.java
@@ -0,0 +1,13 @@
+package com.yvain.clement.gpaiements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpaiementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpaiementsApplication.class, args);
+	}
+
+}
---
+++ gpaiements/src/test/java/com/yvain/clement/gpaiements/GpaiementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvain.clement.gpaiements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpaiementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
