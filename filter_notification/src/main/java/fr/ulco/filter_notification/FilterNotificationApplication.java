6394be86377ae04ca88d4d5c9195ae04d6f799dc
fournier axel <afournie@salle110.kilimandjaro.calais.fr>
2025-10-16 15:36:56 +0200

    Initial commit

---
+++ filter_notification/src/main/java/fr/ulco/filter_notification/FilterNotificationApplication.java
@@ -0,0 +1,13 @@
+package fr.ulco.filter_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterNotificationApplication.class, args);
+	}
+
+}
---
+++ filter_notification/src/test/java/fr/ulco/filter_notification/FilterNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ulco.filter_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
