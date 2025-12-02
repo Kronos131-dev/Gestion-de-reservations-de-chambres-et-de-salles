7ebc508326630949fa20e86e6028df900a36af8c
Kronos <octavenapoleon1er@gmail.com>
2025-12-02 11:54:57 +0100

    feat: Ajout de PrestationEntity

---
+++ gpaiements/src/main/java/com/yvain/clement/gpaiements/persistence/model/PrestationEntity.java
@@ -0,0 +1,28 @@
+import jakarta.persistence.*
+import java.time.LocalDate;
+
+@Entity
+@Table(name ="prestation")
+@Getter
+@Setter
+public class PrestationEntity implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "prestation_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPrestation;
+
+    @ManyToOne
+    @JoinColumn(name = "reservation_id")
+    private Integer idReservation;
+
+    @ManyToOne
+    @JoinColumn(name = "paiement_id")
+    private Integer idPaiement;
+
+    @Column(name = "prix")
+    private Double prix;
+
+}
