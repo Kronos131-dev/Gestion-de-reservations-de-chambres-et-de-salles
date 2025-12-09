604d11ca5175fa9d02f902f572fede7dc38db7c7
AxelFournier19 <axel.fournier190103@gmail.com>
2025-12-09 15:58:53 +0100

    fix: changement type contrôleur EspaceController
    Transformation de EspaceController de RestController en Controller pour permettre l'utilisation de Thymeleaf.

--- filter_notification/src/main/java/fr/ulco/filter_notification/presentation/controllers/EspaceController.java
+++ filter_notification/src/main/java/fr/ulco/filter_notification/presentation/controllers/EspaceController.java
@@ -4,11 +4,12 @@ import fr.ulco.filter_notification.business.services.EspaceService;
 import fr.ulco.filter_notification.persistence.entities.Espace;
 import fr.ulco.filter_notification.presentation.dto.EspaceFilterDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/espaces")
 public class EspaceController {
 
