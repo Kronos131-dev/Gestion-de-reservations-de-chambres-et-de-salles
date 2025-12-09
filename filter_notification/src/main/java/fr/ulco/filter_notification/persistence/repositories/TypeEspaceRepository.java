6621600a778be60659b44bb0f2955743ac775dbd
AxelFournier19 <axel.fournier190103@gmail.com>
2025-12-09 21:17:54 +0100

    feat: ajout dépôt TypeEspaceRepository

---
+++ filter_notification/src/main/java/fr/ulco/filter_notification/persistence/repositories/TypeEspaceRepository.java
@@ -0,0 +1,7 @@
+package fr.ulco.filter_notification.persistence.repositories;
+
+import fr.ulco.filter_notification.persistence.entities.TypeEspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeEspaceRepository extends JpaRepository<TypeEspace, Long> {
+}
