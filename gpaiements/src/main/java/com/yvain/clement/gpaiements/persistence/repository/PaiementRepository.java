58e29b09d183bd21384f8d594ada34f8e296ab59
007personne <003.clementconstant@gmail.com>
2025-12-10 16:59:40 +0100

    feat : ajout methodes de PaiementRepository

--- gpaiements/src/main/java/com/yvain/clement/gpaiements/persistence/repository/PaiementRepository.java
+++ gpaiements/src/main/java/com/yvain/clement/gpaiements/persistence/repository/PaiementRepository.java
@@ -3,10 +3,19 @@ package com.yvain.clement.gpaiements.persistence.repository;
 import com.yvain.clement.gpaiements.persistence.model.PaiementEntity;
 import com.yvain.clement.gpaiements.persistence.model.PaiementStatutEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface PaiementRepository extends JpaRepository<PaiementEntity, Integer>{
-    List<PaiementEntity> findByStatus(PaiementStatutEntity statut);
+
+    List<PaiementEntity> findByStatut(PaiementStatutEntity statut);
+
+    List<PaiementEntity> findByStatutId(Integer statutId);
+
+    List<PaiementEntity> findByDate(LocalDate date);
+    List<PaiementEntity> findByDateBetween(LocalDate debut, LocalDate fin);
 
 }
