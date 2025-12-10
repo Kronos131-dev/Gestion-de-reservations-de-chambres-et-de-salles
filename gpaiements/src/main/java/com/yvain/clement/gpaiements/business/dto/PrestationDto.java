7e39f18912112478ad21b114cd4ad3bd8828b935
007personne <003.clementconstant@gmail.com>
2025-12-10 17:05:40 +0100

    refactor : ajout methodes de PrestationDto

--- gpaiements/src/main/java/com/yvain/clement/gpaiements/business/dto/PrestationDto.java
+++ gpaiements/src/main/java/com/yvain/clement/gpaiements/business/dto/PrestationDto.java
@@ -5,11 +5,9 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class PrestationDto {
-    private Integer idPrestation;
-    private Integer reservationId;
-    private LocalDate paiementId;
-    private BigDecimal prix;
-}
+public record PrestationDto (
+        Integer idPrestation,
+        Integer reservationId,
+        Integer paiementId,
+        BigDecimal prix
+) {}
