ba3b2e3f3b0368d04d123c18841a47e30ec670ac
AxelFournier19 <axel.fournier190103@gmail.com>
2025-12-16 16:26:42 +0100

    fix: utilisation Lombok TypeEspace
    Suppression des getters et setters de la classe TypeEspace, créés automatiquement
    avec les annotations Lombok.

--- filter_notification/src/main/java/fr/ulco/filter_notification/persistence/entities/TypeEspace.java
+++ filter_notification/src/main/java/fr/ulco/filter_notification/persistence/entities/TypeEspace.java
@@ -9,32 +9,8 @@ import lombok.Setter;
 @Table(name="type_espace")
 public class TypeEspace {
 
-    public Long getIdType() {
-        return idType;
-    }
-
-    public void setIdType(Long idType) {
-        this.idType = idType;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getNomEspace() {
-        return nomEspace;
-    }
-
-    public void setNomEspace(String nomEspace) {
-        this.nomEspace = nomEspace;
-    }
-
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="id_type")
     private Long idType;
 
