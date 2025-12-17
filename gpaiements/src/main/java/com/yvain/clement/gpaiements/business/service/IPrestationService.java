62e62063bf65eef21bd96b29b17da16bba18c6a1
007personne <003.clementconstant@gmail.com>
2025-12-18 00:05:20 +0100

    refactor : delete and add des méthodes

--- gpaiements/src/main/java/com/yvain/clement/gpaiements/business/service/IPrestationService.java
+++ gpaiements/src/main/java/com/yvain/clement/gpaiements/business/service/IPrestationService.java
@@ -1,12 +1,12 @@
 package com.yvain.clement.gpaiements.business.service;
 
 import com.yvain.clement.gpaiements.business.dto.PrestationDto;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface IPrestationService {
 
-    PrestationDto create(PrestationDto dto);
     PrestationDto getById(Integer id);
-    List<PrestationDto> getByReservation(Integer reservationId);
-
+    BigDecimal getPiceById(Integer id);
 }
