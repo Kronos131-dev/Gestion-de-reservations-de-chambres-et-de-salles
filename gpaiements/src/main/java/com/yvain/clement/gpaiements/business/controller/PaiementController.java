bece24639bcc405ef28e1dffeb73d9eb2eed227a
Kronos <octavenapoleon1er@gmail.com>
2025-12-17 16:22:27 +0100

    feat: ajout de PaiementController

---
+++ gpaiements/src/main/java/com/yvain/clement/gpaiements/business/controller/PaiementController.java
@@ -0,0 +1,44 @@
+package com.yvain.clement.gpaiements.business.controller;
+
+
+import com.yvain.clement.gpaiements.business.dto.PaiementDto;
+import com.yvain.clement.gpaiements.business.service.IPaiementService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/paiements")
+@CrossOrigin
+public class PaiementController {
+
+    private final IPaiementService paiementService;
+
+    public PaiementController(IPaiementService paiementService) {
+        this.paiementService = paiementService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PaiementDto> create(@RequestBody PaiementDto dto) {
+        PaiementDto paiementDto = paiementService.create(dto);
+        return new ResponseEntity<>(paiementDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PaiementDto> getById  (@PathVariable Integer id) {
+        return ResponseEntity.ok(paiementService.getById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PaiementDto>> getAll() {
+        return ResponseEntity.ok(paiementService.getAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PaiementDto> delete(@PathVariable Integer id) {
+        paiementService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
