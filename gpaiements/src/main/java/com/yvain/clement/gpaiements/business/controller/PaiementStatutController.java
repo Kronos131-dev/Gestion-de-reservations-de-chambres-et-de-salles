c5838c3a3849a4af48600d5e31e109590533cec2
Kronos <octavenapoleon1er@gmail.com>
2025-12-18 15:38:07 +0100

    feat: PaiementStatutController

---
+++ gpaiements/src/main/java/com/yvain/clement/gpaiements/business/controller/PaiementStatutController.java
@@ -0,0 +1,30 @@
+package com.yvain.clement.gpaiements.business.controller;
+
+import com.yvain.clement.gpaiements.business.dto.PaiementStatutDto;
+import com.yvain.clement.gpaiements.business.service.IPaiementStatutService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/statuts")
+@CrossOrigin
+public class PaiementStatutController {
+
+    private final IPaiementStatutService paiementStatutService;
+
+    public PaiementStatutController(IPaiementStatutService paiementStatutService) {
+        this.paiementStatutService = paiementStatutService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PaiementStatutDto>> getAll() {
+        return ResponseEntity.ok(paiementStatutService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PaiementStatutDto> getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(paiementStatutService.getById(id));
+    }
+}
