03bc7204734c41c21f906a9877b18bd9d5f5b52d
AxelFournier19 <axel.fournier190103@gmail.com>
2025-12-18 11:56:54 +0100

    fix: TypeEspaceRepository et EspaceRepository
    Ajout de l'annotation @Repository dans TypeEspaceRepository et
    suppression des imports inutilisés dans EspaceRepository.

--- filter_notification/src/main/java/fr/ulco/filter_notification/persistence/repositories/TypeEspaceRepository.java
+++ filter_notification/src/main/java/fr/ulco/filter_notification/persistence/repositories/TypeEspaceRepository.java
@@ -2,6 +2,8 @@ package fr.ulco.filter_notification.persistence.repositories;
 
 import fr.ulco.filter_notification.persistence.entities.TypeEspace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TypeEspaceRepository extends JpaRepository<TypeEspace, Long> {
 }
