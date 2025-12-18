3836d107dac76357be290339a7f24c4f3544c613
AxelFournier19 <axel.fournier190103@gmail.com>
2025-12-18 12:18:13 +0100

    test: ajout UtilisateurControllerIntegrationTest
    Tests d'intégration du contrôleur pour récupérer tous les utilisateurs.

---
+++ filter_notification/src/test/java/fr/ulco/filter_notification/presentation/controllers/UtilisateurControllerIntegrationTest.java
@@ -0,0 +1,63 @@
+package fr.ulco.filter_notification.presentation.controllers;
+
+import fr.ulco.filter_notification.persistence.entities.Utilisateur;
+import fr.ulco.filter_notification.persistence.repositories.UtilisateurRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UtilisateurControllerIntegrationTest {
+
+    @BeforeEach
+    void setup() {
+        utilisateurRepository.deleteAll();
+
+        u1 = new Utilisateur();
+        u1.setNom("Tim");
+        u1.setPrenom("Vincent");
+        u1.setEmail("Vincent.Tim@gmail.com");
+        u1.setPassword("toto");
+        u1.setTel("0102030406");
+        u1.setDateNaissance(LocalDate.MIN);
+
+        utilisateurRepository.save(u1);
+
+        u2 = new Utilisateur();
+        u2.setNom("Admin");
+        u2.setPrenom("Super");
+        u2.setEmail("admin@site.com");
+        u2.setPassword("tata");
+        u2.setTel("0600000000");
+        u2.setDateNaissance(LocalDate.MIN);
+
+        utilisateurRepository.save(u2);
+    }
+
+    @Test   // GET: /api/utilisateurs
+    void testGetAllUtilisateurs() throws Exception {
+        mockMvc.perform(get("/api/utilisateurs"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2)); // [u1, u2]
+    }
+
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Utilisateur u1, u2;
+}
